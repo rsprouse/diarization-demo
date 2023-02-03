83d93e7a7aa3960b3584aa359a424d906f7cf31a
Ronald L. Sprouse <ronald@berkeley.edu>
2023-02-03 11:23:33 -0800

    Add rttm functions

--- diarize_utils.py
+++ diarize_utils.py
@@ -35,22 +35,27 @@ def mirror_dir(dir1, dir2):
     '''
     Mirror a directory's subdirectories in another directory.
 
-    dir1 : path-like
+    dir1: path-like
     The source directory to be mirrored.
 
-    dir2 : path-like
+    dir2: path-like
     The target directory where new subdirectories will be created.
+    
+    Notes
+    -----
+    This simple implementation is not efficient and may not be suitable for
+    large directory structures or for use with network file systems.
     '''
     dir1 = Path(dir1)
     dir2 = Path(dir2)
     proc = subprocess.run(
-        ['find', '.', '-type', 'd'],
+        ['find', '.', '-type', 'd', '-depth'],
         cwd=dir1,
         capture_output=True,
         text=True
     )
     for name in proc.stdout.splitlines():
-        (dir2 / Path(name)).mkdir(parents=True, exist_ok=True)
+        (dir2 / name).mkdir(parents=True, exist_ok=True)
 
 def prep_audio(infile, outfile, chan='-'):
     '''
@@ -104,3 +109,63 @@ def diarize_df(df, pipeline, num_spkr, wavdir, rttmdir):
         except Exception as e:
             sys.stderr.write(f'Failed to diarize .wav file {wavfile}.')
             sys.stderr.write(e)
+
+def rttm2df(rttmfile):
+    '''
+    Load an .rttm file into a dataframe.
+    '''
+    df = pd.read_csv(
+        rttmfile,
+        sep=' ',
+        header=None,
+        usecols=[1, 3, 4, 7],
+        names=['fid', 't1', 'dur', 'spkr'],
+        converters={'t1': lambda x: float(x) * 1000, 'dur': lambda x: float(x) * 1000},
+        dtype={'fid': 'category', 'spkr': 'category'}
+    )
+    df['t2'] = df['t1'] + df['dur']
+    return df
+
+def buffer_tier(tier, msec):
+    '''
+    Buffer t1 and t2 in a tier by `msec` where possible.
+    # TODO: check for proper handling of starts/ends less than buf_ms from edges
+    '''
+    tier['prev_t2'] = tier['t2'].shift(1, fill_value=np.max([tier.t1.min() - (2 * msec), 0]))
+    tier['next_t1'] = tier['t1'].shift(-1, fill_value=tier.t2.max())
+    # Check for overlapping speaker utterances. 
+    assert(np.all(tier['t1'] < tier['next_t1']))
+    pregap_midpt = tier.loc[:, ['t1', 'prev_t2']].mean(axis=1)
+    tier['t1_buf'] = np.max(
+        [tier['t1'] - msec, pregap_midpt],
+        axis=0
+    )
+    postgap_midpt = tier.loc[:, ['t2', 'next_t1']].mean(axis=1)
+    tier['t2_buf'] = np.min(
+        [tier['t2'] + msec, postgap_midpt],
+        axis=0
+    )
+    return tier
+
+def rttm2eaf(rttms, buffer_ms=None, names={}):
+    '''
+    Convert left/right .rttm files to an .eaf, one tier per speaker per channel.
+    Buffer t1 and t2 in a tier by `buffer_ms` where possible.
+    '''
+    eaf = pympi.Elan.Eaf()
+    eaf.remove_tier('default')
+    eaf.add_language('yid')
+    for chan_s, rttmfile in rttms.items():
+        df = rttm2df(rttmfile)
+        for spkr in df['spkr'].cat.categories.sort_values():
+            try:
+                tiername = names[(chan_s, spkr)]
+            except KeyError:
+                tiername = f'{chan_s}_{spkr}'
+            eaf.add_tier(tiername, language='yid')
+            spkrdf = df[df['spkr'] == spkr].copy()
+            if buffer_ms is not None:
+                spkrdf = buffer_tier(spkrdf, buffer_ms)
+            for row in spkrdf.itertuples():
+                eaf.add_annotation(tiername, int(row.t1_buf), int(row.t2_buf))
+    return eaf
